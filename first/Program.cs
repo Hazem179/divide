089dcf0d053a1af69a0e39ce48ff016c1fc0d7db
hazem <hazemm913@gmail.com>
2019-05-14 23:22:40 +0200

    Initial commit

---
+++ first/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dividing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            double r;
+            Console.WriteLine("enter the first number");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the second number");
+            y= int.Parse(Console.ReadLine());
+            if (y == 0)
+            {
+                Console.WriteLine("can't divide by zero");
+            }
+            else
+            {
+                r = x / y;
+                Console.WriteLine("the result = "+r);
+            }
+
+
+        }
+    }
+}
